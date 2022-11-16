26df8e72cf20a6c01db1885ede604c73316078b0
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-11-16 20:22:05 +0100

    update shee

--- shellvars.h
+++ shellvars.h
@@ -1,1 +1,17 @@
-#include
+#ifndef SHELLVARH
+#define SHELLVARH
+
+/**
+ * struct ShellVar - shellvar list node
+ * @name: name of variable
+ * @val: value of variable
+ * @next: next variable in the list
+ */
+typedef struct ShellVar
+{
+	char *name;
+	char *val;
+	struct ShellVar *next;
+} ShellVar;
+
+#endif
